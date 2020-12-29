6db1f44329e3fce9ccb1a9e98d71ec5ad252423f
sheikhRakib <sheikh2rin@gmail.com>
2020-12-29 15:20:46 +0600

    added packages

--- config/app.php
+++ config/app.php
@@ -165,6 +165,8 @@ return [
         /*
          * Package Service Providers...
          */
+        App\Providers\FortifyServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
 
         /*
          * Application Service Providers...
